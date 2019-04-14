fbd18721b084dc4f3fb3f86a7a5c6582e77effc4
Wirek90 <annawirecka@gmail.com>
2019-04-14 17:01:53 +0200

    test

--- whatevs/settings.py
+++ whatevs/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'orderly',
         'USER': 'USERNAME',
-        'PASSWORD': 'PASSWORD',
+        'PASSWORD': 'p',
         'HOST': 'localhost',
         'PORT': '5432',
     }
