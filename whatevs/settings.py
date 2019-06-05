ef4c174e1ca7c6c986b673cdf35cf3e77266e935
Wirek90 <annawirecka@gmail.com>
2019-06-06 01:46:02 +0200

    add .css and static

--- whatevs/settings.py
+++ whatevs/settings.py
@@ -124,7 +124,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+]
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "whatevs", "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 LOGIN_REDIRECT_URL = ''
